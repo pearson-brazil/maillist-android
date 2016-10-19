61ce0d901933f3efe57ded3f26625243fb226c47
Pearson <w.policiano@gmail.com>
2016-10-19 21:12:29 -0200

    Adiciona Snackbar à LoginActivity

--- MailList/app/src/main/java/br/com/pearson/maillist/UI/LoginActivity.java
+++ MailList/app/src/main/java/br/com/pearson/maillist/UI/LoginActivity.java
@@ -1,7 +1,10 @@
 package br.com.pearson.maillist.UI;
 
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import br.com.pearson.maillist.R;
 
@@ -11,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void showAlert(CharSequence message, View view) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
     }
 }
