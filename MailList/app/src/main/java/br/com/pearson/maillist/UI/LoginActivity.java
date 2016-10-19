9014d2f6075fb3fd5900cb2442e7e36168a396d8
Pearson <w.policiano@gmail.com>
2016-10-19 19:59:17 -0200

    Adiciona Resources

--- MailList/app/src/main/java/br/com/pearson/maillist/UI/LoginActivity.java
+++ MailList/app/src/main/java/br/com/pearson/maillist/UI/LoginActivity.java
@@ -1,7 +1,9 @@
-package br.com.pearson.maillist;
+package br.com.pearson.maillist.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.pearson.maillist.R;
 
 public class LoginActivity extends AppCompatActivity {
 
