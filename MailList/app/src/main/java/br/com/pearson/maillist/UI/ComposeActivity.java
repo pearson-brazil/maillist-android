c20623f634e694031151415ced53e46baccb64d5
Pearson <w.policiano@gmail.com>
2016-10-20 00:29:35 -0200

    Adiciona Activity Compose

---
+++ MailList/app/src/main/java/br/com/pearson/maillist/UI/ComposeActivity.java
@@ -0,0 +1,21 @@
+package br.com.pearson.maillist.UI;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import br.com.pearson.maillist.R;
+
+/**
+ * Created by pearson on 13/09/16.
+ */
+public class ComposeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+    }
+}
