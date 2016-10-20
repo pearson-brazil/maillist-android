10ae77cc83b4d3ef028a76214254ed1141452583
Pearson <w.policiano@gmail.com>
2016-10-20 02:03:18 -0200

    Insere navegação entre InboxActivity e ComposeActivity

--- MailList/app/src/main/java/br/com/pearson/maillist/UI/InboxActivity.java
+++ MailList/app/src/main/java/br/com/pearson/maillist/UI/InboxActivity.java
@@ -1,9 +1,12 @@
 package br.com.pearson.maillist.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.pearson.maillist.R;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class InboxActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class InboxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
+
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.compose)
+    public void composeButtonClicked() {
+        Intent intent = new Intent(this, ComposeActivity.class);
+        startActivity(intent);
+    }
+
+
 }
