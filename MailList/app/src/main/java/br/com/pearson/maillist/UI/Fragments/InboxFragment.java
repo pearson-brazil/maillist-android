a7b69ed0bcb24f9f032c46284da4881512a5194c
Pearson <w.policiano@gmail.com>
2016-10-22 19:13:15 -0200

    Unidade 7- Exemplo 1 - Aula 1 terminada

--- MailList/app/src/main/java/br/com/pearson/maillist/UI/Fragments/InboxFragment.java
+++ MailList/app/src/main/java/br/com/pearson/maillist/UI/Fragments/InboxFragment.java
@@ -21,6 +21,7 @@ import br.com.pearson.maillist.Model.InboxAdapter;
 import br.com.pearson.maillist.Model.InboxInterface;
 import br.com.pearson.maillist.R;
 import br.com.pearson.maillist.UI.ComposeActivity;
+import br.com.pearson.maillist.Utils.Utils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -31,35 +32,6 @@ public class InboxFragment extends Fragment {
     private InboxInterface composeClickListener;
 
     @BindView(R.id.inbox_listView) ListView listView;
-
-    ArrayList<Email> emails = new ArrayList<Email>(Arrays.asList(new Email[]  {
-            new Email("Pellentesque aliquet euismod", "Casey Hamilton", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Sutton Thompson", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Boone Murray", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Sophia Owens", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Bethany Dodson", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Jerry Morgan", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Dickson Snider", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Charity Perry", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Cleo Joseph", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Freeman Keller", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Mercer Meyers", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Carr Nunez", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Bartlett Patrick", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Kirkland Santana", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Christi Dalton", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Sears Velasquez", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Humphrey Crane", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Letitia Warner", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Fannie Suarez", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Luann Harmon", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Janette House", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Stephens Mejia", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Robbie Noel", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Mays Norton", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan."),
-            new Email("Pellentesque aliquet euismod", "Solis Everett", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at vestibulum odio. Vivamus viverra dignissim dictum. Pellentesque aliquet euismod odio eu accumsan.")
-
-    }));
 
     public void setComposeClickListener(InboxInterface composeClickListener) {
         this.composeClickListener = composeClickListener;
@@ -79,10 +51,10 @@ public class InboxFragment extends Fragment {
     }
 
     public void initListView() {
-
-
-        listView.setAdapter(new InboxAdapter(this.getContext(), emails));
+        String json = Utils.loadJSONFromAsset("emails.json");
+        listView.setAdapter(new InboxAdapter(this.getContext(), Utils.parseEmails(json)));
     }
+
 
     @OnClick(R.id.fab)
     public void composeClicked() {
--- MailList/app/src/main/java/br/com/pearson/maillist/Utils/Utils.java
+++ MailList/app/src/main/java/br/com/pearson/maillist/Utils/Utils.java
@@ -7,9 +7,17 @@ import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import br.com.pearson.maillist.Application.MailListApplication;
+import br.com.pearson.maillist.Model.Email;
 import br.com.pearson.maillist.UI.LoginActivity;
 
 /**
@@ -53,5 +61,65 @@ public class Utils {
         editor.remove(key);
         editor.apply();
     }
+
+    public static String loadJSONFromAsset(String fileName) {
+        String json = null;
+        try {
+
+            InputStream inputStream = MailListApplication.getInstance().getApplicationContext().getAssets().open(fileName);
+
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+
+            inputStream.read(buffer);
+            inputStream.close();
+
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return json;
+
+    }
+
+    public static ArrayList<Email> parseEmails (String jsonString){
+
+        // Novo parser (serializa/deserializa)
+        JsonParser parser = new JsonParser();
+
+        // Obtém elemento JSON através parser da jsonString
+        JsonElement element = parser.parse(jsonString);
+
+        // Inicializa um ArrayList para guardar os objetos Email
+        ArrayList<Email> emailsList = new ArrayList<>();
+
+        // Verifica se o elemento JSON contém apenas um JsonObject ou um conjunto deles
+        if (element.isJsonArray()) {
+
+            JsonArray emails = element.getAsJsonArray();
+            for (int i = 0; i < emails.size(); i++) {
+
+                // Obtém o JsonObject correspondente do index i do array email
+                JsonObject emailJson = emails.get(i).getAsJsonObject();
+
+                // Obtém os valores de cada chave do JsonObject
+                String name = emailJson.get("from").getAsString();
+                String subject = emailJson.get("subject").getAsString();
+                String message = emailJson.get("message").getAsString();
+
+                // Instancia e adiciona um novo objeto Email na lista de e-mails
+                emailsList.add(new Email(subject, name, message));
+            }
+        }
+
+        // Retorna a lista de e-mails
+        return  emailsList;
+
+    }
+
+
 
 }
